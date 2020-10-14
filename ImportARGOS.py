37901040b4befb20f087f249bd342c575b5a579c
m12edmon <mee28@duke.edu>
2020-10-14 10:39:06 -0400

    Reads all files in user folder

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -17,7 +17,9 @@ import sys, os, arcpy
 arcpy.env.overwriteOutput =True
 
 # Set input variables (Hard-wired)
-inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
+#inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
+inputFolder = "V:\\ARGOSTRACKING\\Data\\ARGOSdata"
+
 outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
 outputSR = arcpy.SpatialReference(54002)
 
@@ -35,75 +37,85 @@ arcpy.AddField_management(outputFC,"Date","DATE")
 #create an Insert cursor
 cur = arcpy.da.InsertCursor(outputFC,['Shape@','TagID','LC','Date'])
 
-#%% Construct a while loop to iterate through all lines in the datafile
-# Open the ARGOS data file for reading
-inputFileObj = open(inputFile,'r')
-
-# Get the first line of data, so we can use the while loop
-lineString = inputFileObj.readline()
-
-#Start the while loop
-while lineString: 
+#iterate through each ARGOS file in the user supplied folder
+inputFiles = os.listdir(inputFolder)
+for inputFile in inputFiles:
+    #Don't process README.txt file
+    if inputFile == 'README.txt':
+        continue
+    #Add full path to inputFile name
+    inputFile_full = os.path.join(inputFolder, inputFile)
+    print(f"Processing {inputFile}")
     
-    # Set code to run only if the line contains the string "Date: "
-    if ("Date :" in lineString):
-        
-        # Parse the line into a list
-        lineData = lineString.split()
-        
-        # Extract attributes from the datum header line
-        tagID = lineData[0]
-        obsDate= lineData[3]
-        obsTime = lineData[4]
-        obsLC = lineData[7]
-        
-        # Extract location info from the next line
-        line2String = inputFileObj.readline()
-        
-        # Parse the line into a list
-        line2Data = line2String.split()
-        
-        # Extract the date we need to variables
-        obsLat = line2Data[2]
-        obsLon= line2Data[5]
-        
-        # Print results to see how we're doing
-       # print (tagID,obsDate,obsTime,obsLC,"Lat:"+obsLat,"Long:"+obsLon)
-        
-        #Try to convert the coordinates to numbers
-        try:
-
-            # Convert raw coordinate strings to numbers
-            if obsLat[-1] == 'N':
-                obsLat = float(obsLat[:-1])
-            else:
-                obsLat = float(obsLat[:-1] * -1)
-            if obsLon[-1] == 'W':
-                obsLon = float(obsLon[:-1])
-            else:
-                obsLon = float(obsLon[:-1] * -1)
-               
-            # Construct a point object from the feature class
-            obsPoint = arcpy.Point()
-            obsPoint.X = obsLon
-            obsPoint.Y = obsLat
-            
-            # Convert the point to a point geometry object with spatial reference
-            inputSR = arcpy.SpatialReference(4326)
-            obsPointGeom = arcpy.PointGeometry(obsPoint,inputSR)
-            
-            # Create a feature object
-            feature = cur.insertRow((obsPointGeom,tagID,obsLC,obsDate.replace(".","/") + " " + obsTime))
-
-        #Handle any error
-        except Exception as e:
-            print("Error adding record {} to the output".format(tagID))
-        
-    # Move to the next line so the while loop progresses
+#Construct a while loop to iterate through all lines in the datafile
+    # Open the ARGOS data file for reading
+    inputFileObj = open(inputFile_full,'r')
+    
+    # Get the first line of data, so we can use the while loop
     lineString = inputFileObj.readline()
     
-#Close the file object
-inputFileObj.close()
+    #Start the while loop
+    while lineString: 
+        
+        # Set code to run only if the line contains the string "Date: "
+        if ("Date :" in lineString):
+            
+            # Parse the line into a list
+            lineData = lineString.split()
+            
+            # Extract attributes from the datum header line
+            tagID = lineData[0]
+            obsDate= lineData[3]
+            obsTime = lineData[4]
+            obsLC = lineData[7]
+            
+            # Extract location info from the next line
+            line2String = inputFileObj.readline()
+            
+            # Parse the line into a list
+            line2Data = line2String.split()
+            
+            # Extract the date we need to variables
+            obsLat = line2Data[2]
+            obsLon= line2Data[5]
+            
+            # Print results to see how we're doing
+           # print (tagID,obsDate,obsTime,obsLC,"Lat:"+obsLat,"Long:"+obsLon)
+            
+            #Try to convert the coordinates to numbers
+            try:
+    
+                # Convert raw coordinate strings to numbers
+                if obsLat[-1] == 'N':
+                    obsLat = float(obsLat[:-1])
+                else:
+                    obsLat = float(obsLat[:-1] * -1)
+                if obsLon[-1] == 'W':
+                    obsLon = float(obsLon[:-1])
+                else:
+                    obsLon = float(obsLon[:-1] * -1)
+                   
+                # Construct a point object from the feature class
+                obsPoint = arcpy.Point()
+                obsPoint.X = obsLon
+                obsPoint.Y = obsLat
+                
+                # Convert the point to a point geometry object with spatial reference
+                inputSR = arcpy.SpatialReference(4326)
+                obsPointGeom = arcpy.PointGeometry(obsPoint,inputSR)
+                
+                # Create a feature object
+                feature = cur.insertRow((obsPointGeom,tagID,obsLC,obsDate.replace(".","/") + " " + obsTime))
+    
+            #Handle any error
+            except Exception as e:
+                print("Error adding record {} to the output".format(tagID))
+            
+        # Move to the next line so the while loop progresses
+        lineString = inputFileObj.readline()
+        
+    #Close the file object
+    inputFileObj.close()
 
 #Delete the cursor object
 del cur
